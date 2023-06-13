aa17e22c02d8b1a3659683f6ae3bd5e945d98b40
IslamProg <74923378+IslamLessom@users.noreply.github.com>
2023-06-13 23:32:11 +0300

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from "react";
+//router
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+//components 
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import Watch from "./pages/Watch";
 
 export default function App() {
   return (
-    <h1 className='text-3xl font-bold underline'>Hello world</h1>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/watch/:id" element={<Watch />} />
+
+      </Routes>
+    </BrowserRouter>
+  );
 }
