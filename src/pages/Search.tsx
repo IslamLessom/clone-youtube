3ca95ffd712db29e1508fb034905f5dcc418056d
IslamProg <74923378+IslamLessom@users.noreply.github.com>
2023-06-29 11:47:42 +0300

    add

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,7 +1,58 @@
-import React from 'react'
+import React, { useEffect } from "react";
+//components
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { getHomePageVideos } from "../store/reducers/getHomePageVideos";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Spinner from "../components/Spinner";
+import { HomePageVideos } from "../Types";
+import Card from "../components/Card";
+import { clearVideos } from "../store";
+import { useNavigate } from "react-router-dom";
+import { getSearchPageVideos } from "../store/reducers/getSearchPageVideos";
 
 export default function Search() {
+  const navigate = useNavigate()
+
+  const dispatch = useAppDispatch();
+  const videos = useAppSelector((state) => state.youtubeApp.videos);
+  const searchTerm = useAppSelector((state) => state.youtubeApp.searchTerm)
+
+  useEffect(() => {
+    dispatch(clearVideos());
+    if(searchTerm==="") navigate("/")
+    else {
+      dispatch(getSearchPageVideos(false))
+    }
+  }, [dispatch, navigate, searchTerm]);
+
   return (
-    <div>Search</div>
-  )
+    <div className="max-h-screen overflow-hidden">
+      <div style={{ height: "7.5vh" }}>
+        <Navbar />
+      </div>
+      <div className="flex" style={{ height: "92.5vh" }}>
+        <Sidebar />
+        {videos.length ? (
+          <InfiniteScroll
+            dataLength={videos.length}
+            next={() => dispatch(getSearchPageVideos(true))}
+            hasMore={videos.length < 500}
+            loader={<Spinner />}
+            height={650}
+            
+          >
+            <div className="grid gap-y-14 gap-x-8  grid-cols-4 p-8">
+              {videos.map((item: HomePageVideos) => {
+                return <Card data={item} key={item.videoId} />;
+              })}
+            </div>
+          </InfiniteScroll>
+        ) : (
+          <Spinner />
+        )}
+      </div>
+    </div>
+  );
 }
